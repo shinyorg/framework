5b5df1011fdceedcaa81ef73518b835187cedffd
Allan Ritchie <allan.ritchie@gmail.com>
2020-10-12 09:43:21 -0400

    add selected item

--- Shiny.Framework/Scenarios/ListViewModel.cs
+++ Shiny.Framework/Scenarios/ListViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using ReactiveUI;
@@ -14,6 +16,11 @@ namespace Shiny.Scenarios
         {
             this.Load = ReactiveCommand.CreateFromTask(this.Process);
             this.BindBusyCommand(this.Load);
+
+            this.WhenAnyValue(x => x.SelectedItem)
+                .Skip(1)
+                .Subscribe(this.OnSelectedItem)
+                .DisposeWith(this.DestroyWith);
         }
 
 
@@ -30,11 +37,13 @@ namespace Shiny.Scenarios
         }
 
 
-
+        protected virtual void OnSelectedItem(T item) {}
         protected virtual void OnError(Exception ex) {}
+
         protected abstract Task<List<T>> GetData();
         public ICommand Load { get; }
         [Reactive] public List<T> List { get; protected set; }
+        [Reactive] public T SelectedItem { get; set; }
 
 
         public override void OnAppearing()
