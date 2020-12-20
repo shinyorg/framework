b5578032a9f8a9e6c5efd5d69fe967c3de3b129b
Allan Ritchie <allan.ritchie@gmail.com>
2020-12-20 10:04:27 -0500

    Update Extensions.cs

--- Shiny.Framework/Extensions.cs
+++ Shiny.Framework/Extensions.cs
@@ -1,7 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reactive.Linq;
-using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Prism.Navigation;
@@ -27,6 +27,49 @@ namespace Shiny
             }
 
             return result;
+        }
+
+
+        public static Task PickEnumValue<T>(this IDialogs dialogs, string title, Action<T> action)
+        {
+            var keys = Enum.GetNames(typeof(T));
+            var actions = new List<(string Key, Action Action)>(keys.Length);
+
+            foreach (var key in keys)
+            {
+                var value = (T)Enum.Parse(typeof(T), key);
+                actions.Add((key, () => action(value)));
+            }
+            return dialogs.ActionSheet(title, false, actions.ToArray());
+        }
+
+
+        public static ICommand PickEnumValueCommand<T>(this IDialogs dialogs, string title, Action<T> action, IObservable<bool>? canExecute = null) =>
+            ReactiveCommand.CreateFromTask(() => dialogs.PickEnumValue(title, action), canExecute);
+
+
+        public static async Task<bool> RequestAccess(this IDialogs dialogs, Func<Task<AccessState>> request)
+        {
+            var access = await request();
+            return await dialogs.AlertAccess(access);
+        }
+
+
+        public static async Task<bool> AlertAccess(this IDialogs dialogs, AccessState access)
+        {
+            switch (access)
+            {
+                case AccessState.Available:
+                    return true;
+
+                case AccessState.Restricted:
+                    await dialogs.Alert("WARNING: Access is restricted");
+                    return true;
+
+                default:
+                    await dialogs.Alert("Invalid Access State: " + access);
+                    return false;
+            }
         }
 
 
