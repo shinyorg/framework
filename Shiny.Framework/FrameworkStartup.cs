b165a876d0ded78142f773e973d7124f1aa5bcd8
Allan Ritchie <allan.ritchie@gmail.com>
2021-04-10 11:44:54 -0400

    Improve

--- Shiny.Framework/FrameworkStartup.cs
+++ Shiny.Framework/FrameworkStartup.cs
@@ -2,19 +2,32 @@
 using DryIoc;
 using DryIoc.Microsoft.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Prism.DryIoc;
 using Prism.Ioc;
 
 
 namespace Shiny
 {
-    public class FrameworkStartup : ShinyStartup
+    public abstract class FrameworkStartup : ShinyStartup
     {
+        protected IPlatform Platform { get; private set; }
+        ILoggingBuilder? builder;
+
+
+        public override void ConfigureLogging(ILoggingBuilder builder, IPlatform platform)
+            => this.builder = builder;
+
+
+
         public override void ConfigureServices(IServiceCollection services, IPlatform platform)
         {
-            services.UseXfMaterialDialogs();
-            services.UseGlobalCommandExceptionHandler();
+            this.Platform = platform;
+            this.Configure(this.builder!, services);
         }
+
+
+        protected abstract void Configure(ILoggingBuilder builder, IServiceCollection services);
 
 
         internal static IContainer? Container { get; private set; }
