f7d3f64a46d814a08d3fa30da1d57a9aa4b3b68b
Allan Ritchie <allan.ritchie@gmail.com>
2021-05-11 23:32:27 -0400

    minor

--- src/Shiny.Framework/Extensions_Rx.cs
+++ src/Shiny.Framework/Extensions_Rx.cs
@@ -1,14 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reactive;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
-using System.Reflection;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
 using ReactiveUI;
-using Shiny.Impl;
 using Shiny.Infrastructure;
 
 
@@ -16,21 +14,9 @@ namespace Shiny
 {
     public static partial class Extensions
     {
-        public static void UseXfMaterialDialogs(this IServiceCollection services)
-            => services.TryAddSingleton<IDialogs, XfMaterialDialogs>();
-
-        public static void UseGlobalCommandExceptionHandler(this IServiceCollection services, Action<GlobalExceptionHandlerConfig>? configure = null)
-        {
-            services.AddSingleton<GlobalExceptionHandler>();
-            configure?.Invoke(GlobalExceptionHandlerConfig.Instance);
-        }
-
-        public static void UseResxLocalization(this IServiceCollection services, Assembly assembly, string resourceName)
-            => services.AddSingleton<ILocalize>(new ResxLocalize(resourceName, assembly));
-
-
         public static IObservable<string> ObserveTimeAgo<T>(this T npc, Expression<Func<T, DateTimeOffset>> expression, Func<TimeSpan, string> transformer, TimeSpan? interval = null) where T : INotifyPropertyChanged
             => npc.WhenAnyProperty(expression).Select(x => x.ObserveTimeAgo(transformer, interval)).Switch();
+
 
         public static IObservable<string> ObserveTimeAgo(this DateTimeOffset dt, Func<TimeSpan, string> transformer, TimeSpan? intervalTime = null) => Observable
             .Interval(intervalTime ?? TimeSpan.FromSeconds(10))
@@ -62,48 +48,32 @@ namespace Shiny
         }
 
 
-        ///// <summary>
-        ///// Will watch for changes in any observable item in the ObservableCollection
-        ///// </summary>
-        ///// <typeparam name="T"></typeparam>
-        ///// <param name="collection"></param>
-        ///// <returns></returns>
-        //public static IObservable<ItemChanged<T, string>> WhenItemChanged<T>(this ObservableCollection<T> collection)
-        //    where T : INotifyPropertyChanged
-        //    => Observable.Create<ItemChanged<T, string>>(ob =>
-        //    {
-        //        var disp = new CompositeDisposable();
-        //        foreach (var item in collection)
-        //            disp.Add(item.WhenAnyProperty().Subscribe(ob.OnNext));
-
-        //        return disp;
-        //    });
-
-
-        ///// <summary>
-        ///// Will watch for a specific property change with any item in the ObservableCollection
-        ///// </summary>
-        ///// <typeparam name="T"></typeparam>
-        ///// <typeparam name="TRet"></typeparam>
-        ///// <param name="collection"></param>
-        ///// <param name="expression"></param>
-        ///// <returns></returns>
-        //public static IObservable<ItemChanged<T, TRet>> WhenItemValueChanged<T, TRet>(
-        //    this ObservableCollection<T> collection,
-        //    Expression<Func<T, TRet>> expression) where T : INotifyPropertyChanged =>
-        //    Observable.Create<ItemChanged<T, TRet>>(ob =>
-        //    {
-        //        var disp = new CompositeDisposable();
-        //        foreach (var item in collection)
-        //        {
-        //            disp.Add(item
-        //                .WhenAnyProperty(expression)
-        //                .Subscribe(x => ob.OnNext(new ItemChanged<T, TRet>(item, x)))
-        //            );
-        //        }
-
-        //        return disp;
-        //    });
+        /// <summary>
+        /// This will buffer observable pings and timestamp them until the predicate check does not pass
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="thisObs"></param>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        public static IObservable<List<Timestamped<T>>> BufferWhile<T>(this IObservable<T> thisObs, Func<T, bool> predicate)
+            => Observable.Create<List<Timestamped<T>>>(ob =>
+            {
+                var list = new List<Timestamped<T>>();
+                return thisObs
+                    .Timestamp()
+                    .Subscribe(x =>
+                    {
+                        if (predicate(x.Value))
+                        {
+                            list.Add(x);
+                        }
+                        else if (list != null)
+                        {
+                            ob.OnNext(list);
+                            list.Clear();
+                        }
+                    });
+            });
 
 
         public static IDisposable ApplyMaxLengthConstraint<T>(this T npc, Expression<Func<T, string>> expression, int maxLength) where T : INotifyPropertyChanged
---
+++ src/Shiny.Framework/Extensions_Service.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Shiny.Impl;
+
+
+namespace Shiny
+{
+    public static partial class Extensions
+    {
+        public static void UseXfMaterialDialogs(this IServiceCollection services)
+            => services.TryAddSingleton<IDialogs, XfMaterialDialogs>();
+
+        public static void UseGlobalCommandExceptionHandler(this IServiceCollection services, Action<GlobalExceptionHandlerConfig>? configure = null)
+        {
+            services.AddSingleton<GlobalExceptionHandler>();
+            configure?.Invoke(GlobalExceptionHandlerConfig.Instance);
+        }
+
+        public static void UseResxLocalization(this IServiceCollection services, Assembly assembly, string resourceName)
+            => services.AddSingleton<ILocalize>(new ResxLocalize(resourceName, assembly));
+    }
+}
