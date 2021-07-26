cde111b66eaa7b1ce0a3a2eae0ca599e620a355d
Allan Ritchie <allan.ritchie@gmail.com>
2021-07-26 16:48:08 -0400

    stub project

---
+++ src/Shiny.Microsoft.DataSync.Client/Platforms/Shared/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+using System;
+
+
+namespace Shiny
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void UseMicrosoftDataSync(this IServiceCollection services)
+        {
+
+        }
+    }
+}
---
+++ src/Shiny.Microsoft.DataSync.Client/SyncJob.cs
@@ -0,0 +1,14 @@
+﻿using Shiny.Jobs;
+
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Shiny.Microsoft.DataSync.Client
+{
+    public class SyncJob : IJob
+    {
+        public async Task Run(JobInfo jobInfo, CancellationToken cancelToken)
+        {
+        }
+    }
+}
