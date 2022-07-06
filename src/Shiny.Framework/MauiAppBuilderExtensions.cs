bc79dacd2e5e6aa9dc308d04a2d9d2cfc48b72f5
Allan Ritchie <allan.ritchie@gmail.com>
2022-07-05 20:13:33 -0400

    Update

--- src/Shiny.Framework/MauiAppBuilderExtensions.cs
+++ src/Shiny.Framework/MauiAppBuilderExtensions.cs
@@ -6,12 +6,12 @@ namespace Shiny;
 
 public static class MauiAppBuilderExtensions
 {
-	public static MauiAppBuilder UseShinyFramework<TApp>(this MauiAppBuilder builder, Action<PrismAppBuilder> prismBuilder)
-		where TApp : Application, IContainerExtension
+	public static MauiAppBuilder UseShinyFramework<TApp>(this MauiAppBuilder builder, IContainerExtension container, Action<PrismAppBuilder> prismBuilder)
+		where TApp : Application
 	{
 		builder
 			.UseShiny()
-			.UsePrismApp<TApp>(null, prismBuilder);
+			.UsePrismApp<TApp>(container, prismBuilder);
 
 		builder.Services.TryAddSingleton(AppInfo.Current);
 		builder.Services.TryAddSingleton<IDialogs, NativeDialogs>();
