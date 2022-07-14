fcf8dbf97f37ca25bc2c3e88aca9cfa6a179202c
Allan Ritchie <allan.ritchie@gmail.com>
2022-07-13 23:21:44 -0400

    Update NativeDialogs.cs

--- src/Shiny.Framework/Impl/NativeDialogs.cs
+++ src/Shiny.Framework/Impl/NativeDialogs.cs
@@ -15,7 +15,7 @@ public class NativeDialogs : IDialogs
 
 
     public Task<string?> ActionSheet(string title, string? acceptText = null, string? dismissText = null, params string[] options)
-        => this.platform.InvokeOnMainThreadAsync(() => this.prism.Value.DisplayActionSheetAsync(
+        => this.platform.InvokeTaskOnMainThread(() => this.prism.Value.DisplayActionSheetAsync(
             title,
             acceptText,
             dismissText,
@@ -23,13 +23,13 @@ public class NativeDialogs : IDialogs
         ));
 
     public Task Alert(string message, string? title = null, string? dismissText = null)
-        => this.platform.InvokeOnMainThreadAsync(() => this.prism.Value.DisplayAlertAsync(title, message, dismissText ?? "OK"));
+        => this.platform.InvokeTaskOnMainThread(() => this.prism.Value.DisplayAlertAsync(title, message, dismissText ?? "OK"));
 
     public Task<bool> Confirm(string message, string? title = null, string? okText = null, string? cancelText = null)
-        => this.prism.Value.DisplayAlertAsync(title, message, okText, cancelText);
+        => this.platform.InvokeTaskOnMainThread(() => this.prism.Value.DisplayAlertAsync(title, message, okText, cancelText));
 
     public Task<string?> Input(string question, string? title = null, string? acceptText = null, string? dismissText = null, string? placeholder = null, int? maxLength = null)
-        => this.platform.InvokeOnMainThreadAsync(() => this.prism.Value.DisplayPromptAsync(title, question, acceptText, dismissText, placeholder, maxLength ?? -1));
+        => this.platform.InvokeTaskOnMainThread(() => this.prism.Value.DisplayPromptAsync(title, question, acceptText, dismissText, placeholder, maxLength ?? -1));
 
     public Task<IAsyncDisposable> LoadingDialog(string message) => Task.FromResult(AsyncDisposable.Create(async () => { }));
     public Task<IAsyncDisposable> LoadingSnackbar(string message) => Task.FromResult(AsyncDisposable.Create(async () => { }));
