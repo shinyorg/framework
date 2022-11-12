8bbafc02cb97df53460884c52923b01cddfc741d
Allan Ritchie <allan.ritchie@gmail.com>
2022-11-12 10:09:53 -0500

    Create FuncViewModel.cs

---
+++ src/Shiny.Framework/FuncViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Shiny;
+
+
+public class FuncViewModel : ViewModel
+{
+    public FuncViewModel(BaseServices services) : base(services) {}
+
+
+    protected Func<IDisposable, Task>? OnStart { get; set; }
+    protected Action<INavigationParameters, IDisposable>? NavTo { get; set; }
+
+
+    public override async Task InitializeAsync(INavigationParameters parameters)
+    {
+        if (this.OnStart != null)
+            await this.OnStart.Invoke(this.DestroyWith);
+
+        await base.InitializeAsync(parameters);
+    }
+
+
+    public override void OnNavigatedTo(INavigationParameters parameters)
+    {
+        base.OnNavigatedTo(parameters);
+        this.NavTo?.Invoke(parameters, this.DeactivateWith);
+    }
+}
+
