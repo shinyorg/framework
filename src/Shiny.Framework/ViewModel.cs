eb9ac5f0ebbf56060941dd2c01326e2d3f99d66d
Allan Ritchie <allan.ritchie@gmail.com>
2022-11-12 10:10:16 -0500

    Cleanup navigation

--- src/Shiny.Framework/ViewModel.cs
+++ src/Shiny.Framework/ViewModel.cs
@@ -7,7 +7,6 @@ namespace Shiny;
 public abstract class ViewModel : BaseViewModel,
                                   IInitializeAsync,
                                   INavigatedAware,
-                                  IPageLifecycleAware,
                                   IConfirmNavigationAsync
 {
     protected ViewModel(BaseServices services) : base(services)
@@ -16,26 +15,11 @@ public abstract class ViewModel : BaseViewModel,
 
 
     public virtual Task InitializeAsync(INavigationParameters parameters) => Task.CompletedTask;
-    public virtual void OnAppearing() {}
-    public virtual void OnDisappearing() => this.Deactivate();
-
-
-    public virtual void OnNavigatedFrom(INavigationParameters parameters)
-        => this.navSubj?.OnNext((parameters, false));
-
-
-    public virtual void OnNavigatedTo(INavigationParameters parameters)
-        => this.navSubj?.OnNext((parameters, true));
-
-
     public virtual Task<bool> CanNavigateAsync(INavigationParameters parameters)
         => Task.FromResult(true);
 
+    public virtual void OnNavigatedFrom(INavigationParameters parameters)
+        => this.Deactivate();
 
-    Subject<(INavigationParameters, bool)>? navSubj;
-    public IObservable<(INavigationParameters Paramters, bool NavigatedTo)> WhenNavigation()
-    {
-        navSubj ??= new Subject<(INavigationParameters, bool)>();
-        return navSubj.DisposedBy(this.DestroyWith);
-    }
+    public virtual void OnNavigatedTo(INavigationParameters parameters) { }
 }
