aa6285fd0eec10c2f2573db26e150500bb519680
Allan Ritchie <allan.ritchie@gmail.com>
2023-01-04 10:46:35 -0500

    Fix default values on dialogs

--- src/Shiny.Framework/IDialogs.cs
+++ src/Shiny.Framework/IDialogs.cs
@@ -4,10 +4,10 @@ namespace Shiny;
 
 public interface IDialogs
 {
-    Task Alert(string message, string? title = null, string? dismissText = null);
+    Task Alert(string message, string? title = null, string? dismissText = "OK");
     Task<string?> ActionSheet(string title, string? acceptText = null, string? dismissText = null, params string[] options);
-    Task<bool> Confirm(string message, string? title = null, string? acceptText = null, string? dismissText = null);
-    Task<string?> Input(string question, string? title = null, string? acceptText = null, string? dismissText = null, string? placeholder = null, int? maxLength = null, InputKeyboard keyboard = InputKeyboard.Default);
+    Task<bool> Confirm(string message, string? title = null, string? acceptText = "OK", string? dismissText = "Cancel");
+    Task<string?> Input(string question, string? title = null, string? acceptText = "OK", string? dismissText = "Cancel", string? placeholder = null, int? maxLength = null, InputKeyboard keyboard = InputKeyboard.Default);
     Task<bool> Snackbar(string message, int durationMillis = 3000, string? actionText = null);
 }
 
