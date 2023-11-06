e3a13383f1d80230d2620b856899ccea383fa45a
Allan Ritchie <allan.ritchie@gmail.com>
2023-11-05 20:21:49 -0500

    Update Extensions_Rx.cs

--- src/Shiny.Framework/Extensions_Rx.cs
+++ src/Shiny.Framework/Extensions_Rx.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using ReactiveUI;
 
 namespace Shiny;
@@ -9,6 +11,43 @@ namespace Shiny;
 
 public static class RxExtensions
 {
+    /// <summary>
+    /// Allows you to await an observable, while still consuming events
+    /// The await will return when the observable completes or errors
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="observable"></param>
+    /// <param name="onNext"></param>
+    /// <param name="cancelToken"></param>
+    /// <returns></returns>
+    public static async Task ToAsyncWithNotifications<T>(this IObservable<T> observable, Action<T> onNext, CancellationToken cancelToken = default)
+    {
+        IDisposable? sub = null;
+        try
+        {
+            var tcs = new TaskCompletionSource();
+            using var _ = cancelToken.Register(() => tcs.TrySetCanceled());
+
+            sub = observable.Subscribe(
+                onNext,
+                ex => tcs.TrySetException(ex),
+                () => tcs.TrySetResult()
+            );
+
+            await tcs.Task.ConfigureAwait(false);
+        }
+        finally
+        {
+            sub?.Dispose();
+        }
+    }
+
+
+    /// <summary>
+    /// Adds multiple items to a composite disposable
+    /// </summary>
+    /// <param name="disposer"></param>
+    /// <param name="en"></param>
     public static void AddRange(this CompositeDisposable disposer, IEnumerable<IDisposable> en)
     {
         if (en != null)
@@ -17,20 +56,24 @@ public static class RxExtensions
     }
 
 
+    public static IObservable<T> ObserveOnMainThread<T>(this IObservable<T> obs)
+        => obs.ObserveOn(RxApp.MainThreadScheduler);
+
+
     public static IDisposable SubOnMainThread<T>(this IObservable<T> obs, Action<T> onNext)
         => obs
-            .ObserveOn(RxApp.MainThreadScheduler)
+            .ObserveOnMainThread()
             .Subscribe(onNext);
 
 
     public static IDisposable SubOnMainThread<T>(this IObservable<T> obs, Action<T> onNext, Action<Exception> onError)
         => obs
-            .ObserveOn(RxApp.MainThreadScheduler)
+            .ObserveOnMainThread()
             .Subscribe(onNext, onError);
 
 
     public static IDisposable SubOnMainThread<T>(this IObservable<T> obs, Action<T> onNext, Action<Exception> onError, Action onComplete)
         => obs
-            .ObserveOn(RxApp.MainThreadScheduler)
+            .ObserveOnMainThread()
             .Subscribe(onNext, onError, onComplete);
 }
