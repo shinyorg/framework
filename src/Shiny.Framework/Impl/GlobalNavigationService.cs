80dbb136ab59c43d15a8dbea303f061b1ae201c9
Allan Ritchie <allan.ritchie@gmail.com>
2024-03-13 11:39:01 -0400

    Version bumps

--- src/Shiny.Framework/Impl/GlobalNavigationService.cs
+++ src/Shiny.Framework/Impl/GlobalNavigationService.cs
@@ -12,21 +12,13 @@ using Prism.Navigation.Xaml;
 namespace Shiny.Impl;
 
 
-public class GlobalNavigationService : IGlobalNavigationService
+public class GlobalNavigationService(IApplication application, IPlatform platform) : IGlobalNavigationService
 {
-    readonly IApplication application;
-    readonly IPlatform platform;
-
-
-    public GlobalNavigationService(IApplication application, IPlatform platform)
-    {
-        this.application = application;
-        this.platform = platform;
-    }
-
-
     public Task<INavigationResult> GoBackAsync(INavigationParameters parameters)
         => this.Run(nav => nav.GoBackAsync(parameters));
+
+    public Task<INavigationResult> GoBackAsync(string viewName, INavigationParameters parameters)
+        => this.Run(nav => nav.GoBackAsync(viewName, parameters));
 
     public Task<INavigationResult> GoBackToAsync(string name, INavigationParameters parameters)
         => this.Run(nav => nav.NavigateAsync(name, parameters));
@@ -37,15 +29,18 @@ public class GlobalNavigationService : IGlobalNavigationService
     public Task<INavigationResult> NavigateAsync(Uri uri, INavigationParameters parameters)
         => this.Run(nav => nav.NavigateAsync(uri, parameters));
 
+    public Task<INavigationResult> SelectTabAsync(string name, INavigationParameters parameters)
+        => this.Run(nav => nav.SelectTabAsync(name, parameters));
 
-    async Task<INavigationResult> Run(Func<INavigationService, Task<INavigationResult>> func)
+
+    protected virtual async Task<INavigationResult> Run(Func<INavigationService, Task<INavigationResult>> func)
     {
-        var window = this.application.Windows.OfType<Window>().First();
+        var window = application.Windows.OfType<Window>().First();
         var currentPage = MvvmHelpers.GetCurrentPage(window.Page);
         var container = currentPage.GetContainerProvider();
 
         var navService = container.Resolve<INavigationService>();
-        var result = await this.platform.InvokeTaskOnMainThread(() => func(navService));
+        var result = await platform.InvokeTaskOnMainThread(() => func(navService));
         return result;
     }
 }
